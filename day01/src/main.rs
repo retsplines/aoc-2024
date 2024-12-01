b9ef7cf1f35055d50b5f2c8d6320c54255001ba3
Ret <folf@retsplin.es>
2024-12-01 23:21:07 +0000

    Add: Day 1 solution

---
+++ day01/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs::read_to_string;
+
+/**
+ * Reads input.txt, which contains two ASCII whitespace sparated columns of integers.
+ * Sorts the columns independently and totalises the absolute difference the columns for each row.
+ */
+fn main() {
+    
+    // Read the input file line by line, populating our two lists
+    let mut list_a = Vec::new();
+    let mut list_b = Vec::new();
+
+    for line in read_to_string("input.txt").unwrap().lines() {
+
+        // Split into columns
+        let columns_iter = line.split_ascii_whitespace();
+
+        // Map them into integers
+        let mut int_columns = 
+            columns_iter.map(
+                |col| -> u32 { col.parse().expect("Found a noninteger value") }
+            ); 
+
+        list_a.push(int_columns.next().expect("Missing column A"));
+        list_b.push(int_columns.next().expect("Missing column B"));
+    }
+
+    // Assert that the lists hvae the same length
+    assert_eq!(list_a.len(), list_b.len());
+    
+    // Sort the two lists
+    list_a.sort();
+    list_b.sort();
+
+    // Zip the two lists
+    let zip_iter = list_a.iter().zip(list_b.iter());
+
+    // Sum the columns
+    let mut total = 0;
+    for pair in zip_iter {
+        total += pair.0.abs_diff(*(pair.1));
+    }
+    
+    println!("{}", total);
+
+}
